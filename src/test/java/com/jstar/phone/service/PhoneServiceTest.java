039b151b81eb9166dec7168ef4c072b1c5d87bf6
joaquim <mjoaquim12@gmail.com>
2023-12-17 19:11:06 +0400

    Complete bookPhone test with mocks and assertions

--- src/test/java/com/jstar/phone/service/PhoneServiceTest.java
+++ src/test/java/com/jstar/phone/service/PhoneServiceTest.java
@@ -1,15 +1,25 @@
 package com.jstar.phone.service;
 
+import com.jstar.phone.entities.Phone;
+import com.jstar.phone.repository.PhoneRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PhoneServiceTest {
+
+    @Mock
+    private PhoneRepository phoneRepository;
 
     @InjectMocks
     private PhoneService phoneService;
@@ -18,6 +28,12 @@ class PhoneServiceTest {
     void shouldBookPhoneSuccessfully() {
         var model = "Oneplus 9";
         var user = "User1";
+
+        var phone = new Phone(1L, model, null, null);
+        when(phoneRepository.findByModel(model))
+                .thenReturn(Optional.of(phone));
+        when(phoneRepository.save(any(Phone.class)))
+                .thenAnswer(i -> i.getArgument(0));
 
         var bookedPhone = phoneService.bookPhone(model, user);
 
