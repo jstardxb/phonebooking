bf7f7c58ac9d7f7413670917b091cf9f873a4ef5
joaquim <mjoaquim12@gmail.com>
2023-12-17 22:34:34 +0400

    Add failing test for book phone notification

---
+++ src/test/java/com/jstar/phone/service/NotificationServiceTest.java
@@ -0,0 +1,48 @@
+package com.jstar.phone.service;
+
+import com.jstar.phone.config.MessageConfig;
+import com.jstar.phone.dto.PhoneNotification;
+import com.jstar.phone.entities.Phone;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationServiceTest {
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
+
+    @Mock
+    private MessageConfig messageConfig;
+
+    @InjectMocks
+    private NotificationService notificationService;
+
+    @Test
+    void shouldSendBookNotification() {
+        var phone = new Phone(1L, "Oneplus 9", LocalDateTime.now(), "User1");
+
+        when(messageConfig.getInternalExchange())
+                .thenReturn("exchange");
+        when(messageConfig.getInternalNotificationRoutingKeys())
+                .thenReturn("routingKey");
+
+        notificationService.sendBookNotification(phone);
+
+        verify(rabbitTemplate).convertAndSend(
+                eq("exchange"),
+                eq("routingKey"),
+                any(PhoneNotification.class));
+    }
+}
