948f2753cabadb86bb290177fb753e40a7b7c95f
joaquim <mjoaquim12@gmail.com>
2023-12-17 23:25:15 +0400

    Handle MethodArgumentNotValidException

--- src/main/java/com/jstar/phone/exception/GlobalExceptionHandler.java
+++ src/main/java/com/jstar/phone/exception/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package com.jstar.phone.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,5 +26,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PhoneNotBookedException.class)
     public ResponseEntity<Object> handlePhoneNotAvailable(PhoneNotBookedException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex) {
+        var errorMap = new HashMap<String, String>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError ->
+                errorMap.put(fieldError.getField(), fieldError.getDefaultMessage()));
+
+        return errorMap;
     }
 }
