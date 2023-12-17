cdfecf3cabfeee356bf6d3c5c1908b6a049d2c22
joaquim <mjoaquim12@gmail.com>
2023-12-17 23:17:20 +0400

    Add validation annotations for book phone request

--- src/main/java/com/jstar/phone/controller/PhoneController.java
+++ src/main/java/com/jstar/phone/controller/PhoneController.java
@@ -4,6 +4,7 @@ import com.jstar.phone.dto.BookPhoneRequest;
 import com.jstar.phone.dto.ReturnPhoneRequest;
 import com.jstar.phone.entities.Phone;
 import com.jstar.phone.service.PhoneService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class PhoneController {
     private PhoneService phoneService;
 
     @PostMapping("/book")
-    public Phone bookPhone(@RequestBody BookPhoneRequest bookPhoneRequest) {
+    public Phone bookPhone(@RequestBody @Valid BookPhoneRequest bookPhoneRequest) {
         return phoneService.bookPhone(bookPhoneRequest.getModel(), bookPhoneRequest.getBookedBy());
     }
 
--- src/main/java/com/jstar/phone/dto/BookPhoneRequest.java
+++ src/main/java/com/jstar/phone/dto/BookPhoneRequest.java
@@ -1,5 +1,6 @@
 package com.jstar.phone.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BookPhoneRequest {
 
+    @NotBlank(message = "Model cannot be blank")
     private String model;
 
+    @NotBlank(message = "Booked by cannot be blank")
     private String bookedBy;
 }
