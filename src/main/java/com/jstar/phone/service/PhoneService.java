f8d9787482f320d90d67a9af1f92d3b60220f172
joaquim <mjoaquim12@gmail.com>
2023-12-17 19:09:14 +0400

    Refactor bookPhone functionality

--- src/main/java/com/jstar/phone/service/PhoneService.java
+++ src/main/java/com/jstar/phone/service/PhoneService.java
@@ -1,6 +1,7 @@
 package com.jstar.phone.service;
 
 import com.jstar.phone.entities.Phone;
+import com.jstar.phone.repository.PhoneRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,17 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class PhoneService {
 
+    private PhoneRepository phoneRepository;
+
     public Phone bookPhone(String model, String user) {
-        return new Phone(1L, "Nokia", LocalDateTime.now(), "User1");
+        var phone = phoneRepository.findByModel(model)
+                .orElseThrow(()->new IllegalArgumentException("Phone not found"));
+
+        return phoneRepository.save(Phone.builder()
+                .id(phone.getId())
+                .model(model)
+                .bookedAt(LocalDateTime.now())
+                .bookedBy(user)
+                .build());
     }
 }
