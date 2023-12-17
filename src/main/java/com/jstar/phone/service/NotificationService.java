c2b5b7c88fd3173a92631077a17394478b5eae17
joaquim <mjoaquim12@gmail.com>
2023-12-17 22:36:56 +0400

    Implement sendBookNotification in NotificationService

--- src/main/java/com/jstar/phone/service/NotificationService.java
+++ src/main/java/com/jstar/phone/service/NotificationService.java
@@ -1,18 +1,37 @@
 package com.jstar.phone.service;
 
+import com.jstar.phone.config.MessageConfig;
+import com.jstar.phone.dto.PhoneNotification;
+import com.jstar.phone.dto.PhoneNotificationStatus;
 import com.jstar.phone.entities.Phone;
 import lombok.AllArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class NotificationService {
 
+    private MessageConfig messageConfig;
+    private RabbitTemplate rabbitTemplate;
+
     public void sendBookNotification(Phone phone) {
-        //TODO: Implement me
+        sendNotification(phone, PhoneNotificationStatus.BOOKED);
     }
 
     public void sendReturnNotification(Phone phone) {
         //TODO: Implement me
+    }
+
+    private void sendNotification(Phone phone, PhoneNotificationStatus status) {
+        var notification = PhoneNotification.builder()
+                .phone(phone)
+                .status(status)
+                .build();
+
+        rabbitTemplate.convertAndSend(
+                messageConfig.getInternalExchange(),
+                messageConfig.getInternalNotificationRoutingKeys(),
+                notification);
     }
 }
