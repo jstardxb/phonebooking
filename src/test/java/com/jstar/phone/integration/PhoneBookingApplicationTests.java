c878c35d7cbe9b3ecff962b7ddc57b8e2fcf99f7
joaquim <mjoaquim12@gmail.com>
2023-12-18 07:11:58 +0400

    Add integration test

---
+++ src/test/java/com/jstar/phone/integration/PhoneBookingApplicationTests.java
@@ -0,0 +1,57 @@
+package com.jstar.phone.integration;
+
+import com.jstar.phone.dto.BookPhoneRequest;
+import com.jstar.phone.dto.ReturnPhoneRequest;
+import com.jstar.phone.entities.Phone;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class PhoneBookingApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testBookPhone() {
+        var baseUrl = "http://localhost:" + port + "/api/phone/book";
+        var request = new BookPhoneRequest("Oneplus 9", "User1");
+
+        var authRestTemplate = restTemplate.withBasicAuth("admin", "admin");
+
+        var response = authRestTemplate.postForEntity(baseUrl, request, Phone.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("Oneplus 9", response.getBody().getModel());
+        assertEquals("User1", response.getBody().getBookedBy());
+    }
+
+    @Test
+    void testReturnPhone() {
+        var baseUrl = "http://localhost:" + port + "/api/phone/return";
+        var request = new ReturnPhoneRequest("Oneplus 9");
+
+        var authRestTemplate = restTemplate.withBasicAuth("admin", "admin");
+
+        var response = authRestTemplate.postForEntity(baseUrl, request, Phone.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals("Oneplus 9", response.getBody().getModel());
+        assertNull(response.getBody().getBookedAt());
+        assertNull(response.getBody().getBookedBy());
+    }
+}
